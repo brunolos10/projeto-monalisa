a43d11d6f40e596ba3a57767b9a5268c2304bbb8
brunolos10 <160243976+brunolos10@users.noreply.github.com>
2025-03-07 08:11:28 -0300

    s

--- sketch.js
+++ sketch.js
@@ -1,31 +1,32 @@
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(400, 400);
+}
+let olhoX;
+let olhoY;
+
+function draw() {
+  background("#FF5722");
+  fill("green");
+  circle(200, 200, 205); // rosto
+  fill("i");
+  circle(150, 150, 50); // olho esquerdo
+  circle(250, 150, 50); // olho direito
+  line(150, 270, 250, 230); // boca
+  fill("#3F51B5");
+  triangle(200, 180, 170, 220, 220, 220); // nariz
+  line(123, 115, 178, 110); // sobrancelha esquerda
+  line(225, 116, 279, 90); // sobrancelha direita
+  // circle(150,150,10); // pupila esquerda
+  //circle(250,150,10); // pupila direita
+
+  olhoX = map(mouseX, 0, 400, 130, 170);
+  olhoY = map(mouseY, 0, 400, 130, 170);
+
+  circle(olhoX, olhoY, 10); // nova pupila esquerda
+  circle(olhoX + 100, olhoY, 10); //nova pupila direita
+  if (mouseIsPressed) {
+    console.log(mouseX, mouseY);
   }
-  let olhoX;
-  let olhoY;
-  
-  function draw() {
-    background("#FF5722");
-    fill("#03A9F4");
-    circle(200, 200, 300); // rosto
-    fill("white");
-    circle(150, 150, 60); // olho esquerdo
-    circle(250, 150, 60); // olho direito
-    line(150, 270, 250, 235); // boca
-    fill("#3F51B5");
-    triangle(200, 180, 170, 220, 220, 220); // nariz
-    line(123, 115, 178, 113); // sobrancelha esquerda
-    line(225, 116, 279, 106); // sobrancelha direita
-    // circle(150,150,10); // pupila esquerda
-    //circle(250,150,10); // pupila direita
-  
-    olhoX = map(mouseX, 0, 400, 130, 170);
-    olhoY = map(mouseY, 0, 400, 130, 170);
-  
-    circle(olhoX, olhoY, 10); // nova pupila esquerda
-    circle(olhoX + 100, olhoY, 10); //nova pupila direita
-    if (mouseIsPressed) {
-      console.log(mouseX, mouseY);
-    }
-  }
+}
+
   
